b5ab4f299f70893c8313147c359bc601710d2a55
Jan Kramer <jan@jankramer.eu>
2024-12-07 20:36:53 +0100

    Day 06: optimize

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,93 +1,211 @@
-use std::collections::{BTreeMap, HashSet};
+use rustc_hash::FxHashSet;
+use std::cmp::Ordering;
+use std::collections::BTreeSet;
 
 const IN: &str = include_str!("day06.txt");
 
 fn run(input: &str) -> (usize, usize) {
-    let grid: BTreeMap<(i64, i64), char> = input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, row)| {
-            row.chars()
-                .enumerate()
-                .map(move |(x, c)| ((y as i64, x as i64), c))
-        })
-        .collect();
+    let mut start = (0, 0);
+    let mut rows = vec![];
+    let mut cols = vec![];
+    for (r, l) in input.lines().enumerate() {
+        rows.push(BTreeSet::new());
+        for (c, v) in l.chars().enumerate() {
+            if r == 0 {
+                cols.push(BTreeSet::new());
+            }
 
-    let (start_pos, _) = grid.iter().find(|(_, &c)| c == '^').unwrap();
+            if v == '#' {
+                rows[r].insert(c);
+                cols[c].insert(r);
+            }
 
-    let mut pos = *start_pos;
-    let mut dir: (i64, i64) = (-1, 0);
-
-    let mut visited: HashSet<(i64, i64)> = HashSet::new();
-    visited.insert(pos);
-
-    loop {
-        let next = (pos.0 + dir.0, pos.1 + dir.1);
-
-        let Some(next_char) = grid.get(&next).copied() else {
-            break;
-        };
-
-        if next_char == '#' {
-            dir = rotate(dir);
-        } else {
-            visited.insert(next);
-            pos = next;
-        }
-    }
-
-    let mut obstacles: HashSet<(i64, i64)> = HashSet::new();
-    for x in &visited {
-        let mut pos = *start_pos;
-        let mut dir: (i64, i64) = (-1, 0);
-
-        let mut visited_in_dir: HashSet<((i64, i64), (i64, i64))> = HashSet::new();
-        let mut visited2: HashSet<(i64, i64)> = HashSet::new();
-        visited2.insert(pos);
-
-        loop {
-            let next = (pos.0 + dir.0, pos.1 + dir.1);
-
-            let Some(next_char) = grid.get(&next).copied() else {
-                break;
-            };
-
-            if &next == x || next_char == '#' {
-                dir = rotate(dir);
-            } else {
-                visited2.insert(next);
-                pos = next;
-                if !visited_in_dir.insert((pos, dir)) {
-                    obstacles.insert(*x);
-
-                    break;
-                }
+            if v == '^' {
+                start = (r, c);
             }
         }
     }
 
-    (visited.len(), obstacles.len())
+    let Result::Exit(path) = walk_grid(&cols, &rows, start, Heading::N) else {
+        panic!("part 1 should exit");
+    };
+
+    let full_path: Vec<_> = path
+        .windows(2)
+        .flat_map(|pts| points_between(pts[0].0, pts[1].0).map(|p| (p, pts[0].1)))
+        .collect();
+
+    let p1 = full_path
+        .iter()
+        .map(|x| x.0)
+        .collect::<FxHashSet<_>>()
+        .len();
+
+    let mut p2: FxHashSet<(usize, usize)> = FxHashSet::default();
+    let mut visited = FxHashSet::default();
+    for (p, dir) in full_path {
+        if !visited.insert(p) {
+            continue;
+        }
+
+        rows[p.0].insert(p.1);
+        cols[p.1].insert(p.0);
+
+        let start = match dir {
+            Heading::N => (p.0 + 1, p.1),
+            Heading::E => (p.0, p.1 - 1),
+            Heading::S => (p.0 - 1, p.1),
+            Heading::W => (p.0, p.1 + 1),
+        };
+
+        let result = walk_grid(&cols, &rows, start, dir.rotate());
+        if matches!(result, Result::Loop) {
+            p2.insert(p);
+        }
+
+        rows[p.0].remove(&p.1);
+        cols[p.1].remove(&p.0);
+    }
+
+    (p1, p2.len())
 }
 
-fn rotate(dir: (i64, i64)) -> (i64, i64) {
-    match dir {
-        (1, 0) => (0, -1),
-        (0, -1) => (-1, 0),
-        (-1, 0) => (0, 1),
-        (0, 1) => (1, 0),
-        _ => panic!(),
+fn walk_grid(
+    cols: &[BTreeSet<usize>],
+    rows: &[BTreeSet<usize>],
+    start: (usize, usize),
+    dir: Heading,
+) -> Result {
+    let mut path = vec![(start, dir)];
+    let mut visited = FxHashSet::default();
+    let mut pos = start;
+    let mut dir = dir;
+    let height = rows.len();
+    let width = cols.len();
+    loop {
+        let next = match dir {
+            Heading::N => cols[pos.1].iter().rev().skip_while(|&&r| r > pos.0).next(),
+            Heading::S => cols[pos.1].iter().skip_while(|&&r| r < pos.0).next(),
+            Heading::E => rows[pos.0].iter().skip_while(|&&c| c < pos.1).next(),
+            Heading::W => rows[pos.0].iter().rev().skip_while(|&&c| c > pos.1).next(),
+        }
+        .copied();
+
+        let Some(next) = next else {
+            path.push((
+                match dir {
+                    Heading::N => (0, pos.1),
+                    Heading::S => (height - 1, pos.1),
+                    Heading::E => (pos.0, width - 1),
+                    Heading::W => (pos.0, 0),
+                },
+                dir,
+            ));
+
+            break;
+        };
+
+        let next_pos = match dir {
+            Heading::N => (next + 1, pos.1),
+            Heading::S => (next - 1, pos.1),
+            Heading::E => (pos.0, next - 1),
+            Heading::W => (pos.0, next + 1),
+        };
+
+        pos = next_pos;
+        dir = dir.rotate();
+
+        path.push((next_pos, dir));
+
+        if !visited.insert((next_pos, dir)) {
+            return Result::Loop;
+        }
+    }
+
+    Result::Exit(path)
+}
+
+#[derive(Debug)]
+enum Result {
+    Loop,
+    Exit(Vec<((usize, usize), Heading)>),
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+enum Heading {
+    N,
+    E,
+    S,
+    W,
+}
+
+impl Heading {
+    pub fn rotate(&self) -> Self {
+        match self {
+            Heading::N => Heading::E,
+            Heading::E => Heading::S,
+            Heading::S => Heading::W,
+            Heading::W => Heading::N,
+        }
     }
 }
 
+fn points_between(p1: (usize, usize), p2: (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
+    let dr = p1.0.cmp(&p2.0);
+    let dc = p1.1.cmp(&p2.1);
+
+    let mut done = false;
+    let (mut r, mut c) = p1;
+
+    std::iter::from_fn(move || {
+        if done {
+            return None;
+        }
+
+        let result = Some((r, c));
+
+        if r == p2.0 && c == p2.1 {
+            done = true;
+            return result;
+        }
+
+        match (dr, dc) {
+            (Ordering::Greater, _) => {
+                r -= 1;
+            }
+            (Ordering::Less, _) => {
+                r += 1;
+            }
+            (_, Ordering::Greater) => {
+                c -= 1;
+            }
+            (_, Ordering::Less) => {
+                c += 1;
+            }
+            _ => unreachable!(),
+        };
+
+        result
+    })
+}
+
 fn main() {
-    let now = std::time::Instant::now();
     let (p1, p2) = run(IN);
-    let elapsed = now.elapsed();
+
+    assert_eq!(5453, p1);
+    assert_eq!(2188, p2);
 
     println!("Day 06\n======");
     println!("Part 1: {p1}");
     println!("Part 2: {p2}");
-    println!("{}µs\n", elapsed.as_micros());
+
+    let now = std::time::Instant::now();
+    for _ in 0..10 {
+        run(IN);
+    }
+    let elapsed = now.elapsed();
+
+    println!("{}µs\n", elapsed.as_micros() / 10);
 }
 
 #[cfg(test)]
