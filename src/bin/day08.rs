1563bf506d0df58c4490362e8704ac1feaa99fb1
Jan Kramer <jan@jankramer.eu>
2024-12-08 09:15:13 +0100

    Day 08: optimize

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,53 +1,57 @@
-use itertools::Itertools;
-use std::collections::{BTreeMap, HashSet};
+const IN: &[u8] = include_bytes!("day08.txt");
 
-const IN: &str = include_str!("day08.txt");
+fn run(input: &[u8]) -> (usize, usize) {
+    let width: i64 = input.iter().position(|&c| c == b'\n').unwrap() as i64;
+    let height: i64 = input.trim_ascii_end().len() as i64 / width;
 
-fn run(input: &str) -> (usize, usize) {
-    let grid: BTreeMap<(i64, i64), char> = input
-        .lines()
-        .enumerate()
-        .flat_map(|(r, row)| {
-            row.chars()
-                .enumerate()
-                .filter(|(_, c)| *c != '.')
-                .map(move |(c, v)| ((r as i64, c as i64), v))
-        })
-        .collect();
+    let mut map: [Vec<(i64, i64)>; 128] = [const { Vec::new() }; 128];
+    for (idx, &char) in input.iter().filter(|&&c| c != b'\n').enumerate() {
+        if char == b'.' {
+            continue;
+        }
 
-    let width = input.lines().next().unwrap().len() as i64;
-    let height = input.lines().count() as i64;
+        map[char as usize].push((idx as i64 % width, idx as i64 / width));
+    }
 
-    let mut p1 = HashSet::new();
-    let mut p2 = HashSet::new();
+    let mut p1 = vec![false; (width * height + 1) as usize];
+    let mut p2 = vec![false; (width * height + 1) as usize];
 
-    let add_if_valid = |foo: &mut HashSet<(i64, i64)>, (r, c): (i64, i64)| {
-        (r >= 0 && c >= 0 && r < height && c < width).then(|| foo.insert((r, c)))
+    let insert = |foo: &mut Vec<bool>, p: &(i64, i64)| {
+        if p.0 >= 0 && p.1 >= 0 && p.0 < height && p.1 < width {
+            foo[(p.1 + p.0 * width) as usize] = true;
+            true
+        } else {
+            false
+        }
     };
 
-    for (_, pts) in grid.into_iter().map(|(k, v)| (v, k)).into_group_map() {
-        for (a, b) in pts.into_iter().tuple_combinations() {
-            let dr = b.0 - a.0;
-            let dc = b.1 - a.1;
+    for indices in &map {
+        for a in indices {
+            for b in indices {
+                if a == b {
+                    continue;
+                }
 
-            add_if_valid(&mut p1, (a.0 - dr, a.1 - dc));
-            add_if_valid(&mut p1, (b.0 + dr, b.1 + dc));
+                p2[(a.1 + a.0 * width) as usize] = true;
 
-            for i in 0.. {
-                if add_if_valid(&mut p2, (a.0 - i * dr, a.1 - i * dc)).is_none() {
-                    break;
-                };
-            }
+                let dr = b.0 - a.0;
+                let dc = b.1 - a.1;
 
-            for i in 0.. {
-                if add_if_valid(&mut p2, (b.0 + i * dr, b.1 + i * dc)).is_none() {
-                    break;
+                if insert(&mut p1, &(a.0 - dr, a.1 - dc)) {
+                    for i in 1.. {
+                        if !insert(&mut p2, &(a.0 - i * dr, a.1 - i * dc)) {
+                            break;
+                        };
+                    }
                 }
             }
         }
     }
 
-    (p1.len(), p2.len())
+    (
+        p1.iter().map(|&x| x as usize).sum(),
+        p2.iter().map(|&x| x as usize).sum(),
+    )
 }
 
 fn main() {
@@ -58,18 +62,18 @@ fn main() {
     println!("Part 2: {p2}");
 
     let now = std::time::Instant::now();
-    for _ in 0..1000 {
+    for _ in 0..100000 {
         run(IN);
     }
     let elapsed = now.elapsed();
-    println!("{}µs\n", elapsed.as_micros() / 1000);
+    println!("{}ns\n", elapsed.as_nanos() / 100000);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const T1: &str = r#"............
+    const T1: &[u8] = b"............
 ........0...
 .....0......
 .......0....
@@ -80,7 +84,7 @@ mod tests {
 ........A...
 .........A..
 ............
-............"#;
+............";
 
     #[test]
     fn test() {
